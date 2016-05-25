70cdadde4c73f2c95c587ad4ffcf6107b2472a85
Ayush Sharma <deefunktify@gmail.com>
2016-05-26 04:09:25 +1000

    - finally horizontal stabilization over marker working - vertical distance maintained over marker - PD control system

--- ardrone.py
+++ ardrone.py
@@ -9,12 +9,6 @@ import shlex
 import math
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
-
-
-
-
-
-# import sys
 from sensor_msgs.msg import Image
 from ardrone_autonomy.msg import Navdata
 
@@ -36,21 +30,15 @@ r2d = 180/math.pi
 d2r = math.pi/180
 
 
-
-
 def initdrone():
     try:
         launchcommand = shlex.split('roslaunch ardrone_autonomy ardrone.launch')
         roscore = subprocess.Popen(launchcommand)
-        # rospy.sleep(3)
-        # ardronedriver = subprocess.Popen('rosrun ardrone_autonomy ardrone_driver')
-
         rospy.sleep(10.)
+        rospy.init_node('dronetest', anonymous=True)
+        return roscore
     except RuntimeError:
         print('invalid launch')
-
-    rospy.init_node('dronetest',anonymous=True)
-
 
 
 class BasicDroneController(object):
@@ -68,19 +56,17 @@ class BasicDroneController(object):
 
     # retrive and store data??
     def GetNavdata(self,data):
+        self.marker.time = data.tm
         self.markercount = data.tags_count
         if self.markercount == 1:
             self.marker.cx = data.tags_xc[0]
             self.marker.cy = data.tags_yc[0]
             self.marker.cvec = np.array([self.marker.cx, self.marker.cy])
             self.marker.theta = -data.tags_orientation[0]
-            # print(self.marker.theta)
-            # print (self.marker.cx ,self.marker.cy)
             self.marker.width = data.tags_width[0]
             self.marker.height = data.tags_height[0]
-            self.marker.time = data.tm
-            # self.marker = {'cx': data.tags_xc[0], 'cy' : data.tags_yc[0], 'width' : data.tags_width[0], 'height' : data.tags_height[0]}
-            # return store
+            self.marker.dist = data.tags_distance[0]
+
 
 class createmarker():
     def __init__(self):
@@ -91,6 +77,7 @@ class createmarker():
         height = -1
         theta = -1.0
         time = 0
+        dist = 0
 
 class createrect():
     def __init__(self):
@@ -105,28 +92,14 @@ def drawmarker(rows,cols,marker):
     # assume height and width doesnt need to /1000* cols||rows
     Hlong = math.sqrt((marker.width*roundel_ratio)**2 + (float(marker.height)/2)**2)
     Hshort = math.sqrt((marker.width*(1-roundel_ratio))**2+(float(marker.height)/2)**2)
-    # width = marker.width*cols/(1000) # not valid for orientated
-    # height= marker.height*rows/(1000)
-    # pt1 top right, pt2 top left, pt3 bottom left, pt4 bottom right
-
-    # pt1x = marker.cx*cols/1000 - width
-    # pt1y = marker.cy*rows/1000 + height
-    #
-    # pt2x = marker.cx*cols/1000 + width
-    # pt2y = marker.cy*rows/1000 - height
-
     pt1x = (Hlong*math.cos(marker.theta*d2r+roundel_alpha)) + (float(marker.cx)/1000)*cols
     pt1y = (Hlong*math.sin(marker.theta*d2r+roundel_alpha)) + (float(marker.cy)/1000)*rows
-
     pt2x = (Hshort*math.cos(marker.theta*d2r+roundel_beta)) + (float(marker.cx)/1000)*cols
     pt2y = (Hshort*math.sin(marker.theta*d2r+roundel_beta)) + (float(marker.cy)/1000)*rows
-
     pt3x = (Hshort*math.cos(marker.theta*d2r-roundel_beta)) + (float(marker.cx)/1000)*cols
     pt3y = (Hshort*math.sin(marker.theta*d2r-roundel_beta)) + (float(marker.cy)/1000)*rows
-
     pt4x = (Hlong*math.cos(marker.theta*d2r-roundel_alpha)) + (float(marker.cx)/1000)*cols
     pt4y = (Hlong*math.sin(marker.theta*d2r-roundel_alpha)) + (float(marker.cy)/1000)*rows
-
     rect = createrect()
     rect.pt1 = (int(pt1x),int(pt1y))
     rect.pt2 = (int(pt2x),int(pt2y))
@@ -141,71 +114,82 @@ def seeimage(ros_image,marker):
         cv_image = bridge.imgmsg_to_cv2(ros_image,"bgr8") #rgb
     except CvBridgeError as e:
         print(e)
-
     if me.markercount == 1:
         (rows,cols,channels) = cv_image.shape
-        # rect = drawmarker(rows,cols,marker)
         colour = (255,0,0)
-        # cv2.rectangle(cv_image,rect.pt1,rect.pt2,colour,3)
-        # cv2.circle(cv_image,rect.center,3,colour)
-        ################ EDITED Y VALUE
-        # if marker.cy < 180:
-        #     cyedit = (180-marker.cy)*180/135
-        #     cyedit = cyedit-135
-        # else:
-        #     cyedit = (marker.cy-180)*180/135
-        #     cyedit = cyedit + 135
-        #
-        # marker.cy = cyedit
         rect = drawmarker(rows,cols,marker)
-        # not rotated
-        # cv2.rectangle(cv_image,rect.pt1,rect.pt2,colour,3)
-        # rotated rectangle
         cv2.line(cv_image,rect.pt1,rect.pt2,colour)
         cv2.line(cv_image,rect.pt2,rect.pt3,colour)
         cv2.line(cv_image,rect.pt3,rect.pt4,colour)
         cv2.line(cv_image,rect.pt4,rect.pt1,colour)
-
-
         cv2.circle(cv_image,(marker.cx*cols/1000,marker.cy*rows/1000),3,colour)
-
     cv2.imshow("Image window", cv_image)  # cv_image is a matrix
     cv2.waitKey(3)
-        # cv2.destroyAllWindows()
-        # k = cv2.waitKey(3)  & 0xFF   # press a key in that time to keep it? (64 bit machine?)
-        # if k == 27:         # wait for ESC key to exit
-        #     cv2.destroyAllWindows()
-        # else: # save and destroy
-        #     cv2.imwrite('TestingRoundel.png',cv_image)
-        #     cv2.destroyAllWindows()
-    # cv2.waitKey(3) # wait three seconds?
 
-
-def Pcontrol(setpos,cpos,ctime,ppos,ptime):
-    vmaxP = 0.4
-    vmaxD = 0
-    maxdev = np.array([500., 500.])
-    dt = (ctime - ptime)/1000000
-    derror = ((setpos - cpos) - (setpos - ppos))/dt
-    error = (setpos-cpos)/maxdev
-    if np.amax(abs(error)) < 0.1:
-        output = np.array([0,0])
-    else:
-        # output = -K*error
-        output = -vmaxP*error + vmaxD*derror/maxdev
-    return output
-
+# takes in [x, y] coordinates of marker, and executes a moveForward or moveBack based on error in position
+def followImage(marker, height, time, previouspos, previoustime):
+    center = np.array([500., 500.])
+    kp = 1/1500.
+    kd = 0.07
+    errorInXPos = float((500 - marker[1]))  # since the errors in camera image and drone reference are inverted, we use
+    errorInYPos = float((500 - marker[0]))  # the opposite vector elements in marker[]
+    dt = (time - previoustime)
+    # errorNow - prevError divided by the time
+    derror = ((center - marker) - (center - previouspos)) / dt
+    if math.isnan(derror[0]):
+        derror = [0., 0.]
+    pdControlX = float(kp*errorInXPos + kd*derror[1])
+    pdControlY = float(kp*errorInYPos + kd*derror[0])
+    print "move in x: " + str(pdControlX) + "\nmove in y: " + str(pdControlY)
+    print "Our height is; " + str(height)
+    print "The change in Time: " + str(dt)
+    p1sec = rospy.Duration(0, 10000000)
+    rospy.sleep(p1sec)
+    control.moveXY(pdControlX, pdControlY, height)
+    return np.array([marker, time])
 
 class twistMatrix:
 
     def __init__(self, movePublisher):
         self.movePub = movePublisher
 
-    def moveForward(self):
+    def moveXY(self, x, y, height):
+        cmd = Twist()
+        cmd.linear.x = 0.25*x
+        cmd.linear.y = 0.25*y
+        cmd.linear.z = 0
+        cmd.angular.x = 0
+        cmd.angular.y = 0
+        cmd.angular.z = 0
+        if height < 150:
+            cmd.linear.z = 0.1
+        self.movePub.publish(cmd)
+
+    def autoHover(self):
         cmd = Twist()
         cmd.linear.z = 0
+        cmd.linear.x = 0
+        cmd.linear.y = 0
+        cmd.angular.x = 0
+        cmd.angular.y = 0
+        cmd.angular.z = 0
+        self.movePub.publish(cmd)
+
+    def hoverNoAuto(self):
+        cmd = Twist()
+        cmd.linear.z = 0
+        cmd.linear.x = 0
+        cmd.linear.y = 0
+        cmd.angular.x = 0.1
+        cmd.angular.y = 0.2
+        cmd.angular.z = 0
+        self.movePub.publish(cmd)
+
+    def moveForward(self):
+        cmd = Twist()
         cmd.linear.x = 0.2
-        cmd.linear.y = 0.1
+        cmd.linear.y = 0
+        cmd.linear.z = 0
 
         cmd.angular.x = 0
         cmd.angular.y = 0
@@ -214,9 +198,11 @@ class twistMatrix:
 
     def moveBack(self):
         cmd = Twist()
+        cmd.linear.x = -0.3
+        cmd.linear.y = 0
         cmd.linear.z = 0
-        cmd.linear.x = -0.25
-        cmd.linear.y = 0.05
+
+
 
         cmd.angular.x = 0
         cmd.angular.y = 0
@@ -269,9 +255,9 @@ class twistMatrix:
 
     def moveLeft(self):
         cmd = Twist()
-        cmd.linear.z = 0
         cmd.linear.x = 0
         cmd.linear.y = 0.2
+        cmd.linear.z = 0
 
         cmd.angular.x = 0
         cmd.angular.y = 0
@@ -280,45 +266,18 @@ class twistMatrix:
 
     def moveRight(self):
         cmd = Twist()
-        cmd.linear.z = 0
         cmd.linear.x = 0
         cmd.linear.y = -0.2
+        cmd.linear.z = 0
 
         cmd.angular.x = 0
         cmd.angular.y = 0
         cmd.angular.z = 0
         self.movePub.publish(cmd)
 
-    def autoHover(self):
-        cmd = Twist()
-        cmd.linear.z = 0
-        cmd.linear.x = 0
-        cmd.linear.y = 0
-        cmd.angular.x = 0
-        cmd.angular.y = 0
-        cmd.angular.z = 0
-        self.movePub.publish(cmd)
 
-    def hoverNoAuto(self):
-        cmd = Twist()
-        cmd.linear.z = 0
-        cmd.linear.x = 0
-        cmd.linear.y = 0
-        cmd.angular.x = 0.1
-        cmd.angular.y = 0.2
-        cmd.angular.z = 0
-        self.movePub.publish(cmd)
 
-    def xyinput(self, vec):
-        cmd = Twist()
-        cmd.linear.z = 0
-        # cmd.linear.x = -vec[1]
-        # cmd.linear.y = -vec[0]-(0.4*vec[1])
-        cmd.angular.x = 0
-        cmd.angular.y = 0
-        cmd.angular.z = 0
-        [cmd.linear.x, cmd.linear.y] = self.transform2DroneRef(np.array(vec))
-        self.movePub.publish(cmd)
+
 
     def land(self, landPub):
         self.autoHover()
@@ -335,63 +294,42 @@ class twistMatrix:
         return vectorXYdrone
 
 if __name__ == '__main__':
-    initdrone()
+    this = initdrone()
     me = BasicDroneController()  # should automatically call GetNavdata when something in received in subscriber
     subNavdata = rospy.Subscriber('/ardrone/navdata', dronemsgs.Navdata, me.GetNavdata)
     image_sub = rospy.Subscriber("/ardrone/image_raw", Image, seeimage, me.marker)
     pubReset = rospy.Publisher("/ardrone/reset", stMsg.Empty, queue_size=1)
     pubTakeoff = rospy.Publisher("/ardrone/takeoff", stMsg.Empty, queue_size=1)
-    pubLand = rospy.Publisher("/ardrone/land", stMsg.Empty,queue_size=1)
-    pubCmd = rospy.Publisher("/cmd_vel", Twist, queue_size = 6)
+    pubLand = rospy.Publisher("/ardrone/land", stMsg.Empty, queue_size=1)
+    pubCmd = rospy.Publisher("/cmd_vel", Twist, queue_size=6)
     EmptyMessage = stMsg.Empty()
-    prevMarker = createmarker()
-    currentMarker = createmarker()
     five_seconds = rospy.Duration(5)
     two_seconds = rospy.Duration(2)
 
-
-
-##### control sequence
+##### takeoff control sequence
     rospy.sleep(two_seconds)
-    pubTakeoff.publish(EmptyMessage)
-    rospy.sleep(two_seconds)
+    # pubTakeoff.publish(EmptyMessage)
     control = twistMatrix(pubCmd)
-    #control.moveForward()
-    # rospy.sleep(rospy.Duration(2))
-    # control.hover()
-    # rospy.sleep(rospy.Duration(1))
-    # control.moveBack()
-    # rospy.sleep(rospy.Duration(2))
-    # control.land(pubLand)
-    # control.autoHover()
-    # rospy.sleep(rospy.Duration(3))
+
+    # stabilize after Launch,
+    rospy.sleep(rospy.Duration(1))
     control.hoverNoAuto()
-    loop = 1
-    randomFlag = False
+    rospy.sleep(rospy.Duration(3))
+    control.moveXY(0,0,0.1) # increase hover height to widen field of view
+    rospy.sleep(rospy.Duration(1))
 
-    while not rospy.is_shutdown() :
-        if loop < 300 and me.markercount==1:
-
-            if randomFlag is False:
-                #prevMarker = getattr(me.marker, 'time') #
-                prevtime = getattr(me.marker, 'time')  #
-                prevpos = getattr(me.marker,'cvec')
-                randomFlag = True
+    # while not rospy.is_shutdown():
+    previousMarker = np.array([500, 500])
+    prevTime = getattr(me.marker, 'time')
+    previousMarkerObject = np.array([previousMarker, prevTime])
+    while 1:
+        try:
+            if me.markercount is 1:
+                thisfunction = followImage(getattr(me.marker, 'cvec'), getattr(me.marker, 'dist'),getattr(me.marker, 'time'), previousMarkerObject[0], previousMarkerObject[1])
+                previousMarkerObject = thisfunction
+                # rospy.spin()
             else:
-                #currentMarker = getattr(me.marker, 'time') #
-                currtime = getattr(me.marker, 'time')  #
-                currpos = getattr(me.marker, 'cvec')
-                vel = Pcontrol(np.array([500,500]), currpos,currtime,prevpos,prevtime)
-                prevtime = currtime
-                prevpos = currpos
-                # print (-vel[1],-vel[0])
-                control.xyinput(vel)
-            rospy.sleep(rospy.Duration(0.1)) # wait a bit
-            loop += 1
-        elif loop < 300:
-            randomFlag = False
-            control.hoverNoAuto()
-            rospy.sleep(rospy.Duration(0.1)) # wait a bit
-            loop += 1
-        else:
+                control.hoverNoAuto()
+        except:
             control.land(pubLand)
+            this.kill()
